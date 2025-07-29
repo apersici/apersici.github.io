7829be02c54acd8f1d51908b8f0674502d0b07a4
apersici <48560969+apersici@users.noreply.github.com>
2025-07-29 12:08:34 +0000

    Deploying to gh-pages from @ apersici/apersici.github.io@a8e86f61503f6db41079ef167f0fb17ff38df4d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Taught courses.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
